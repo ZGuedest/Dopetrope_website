55de236e6f456f9b8dba09cbff9b92eccdc52166
Zguedest <zucelys.zge@gmail.com>
2023-01-17 14:05:11 +0100

    javascript de rango en formulario

--- funcion.js
+++ funcion.js
@@ -3,7 +3,8 @@
 let button= document.getElementById("form_button");
 
 
-button.addEventListener("click",function(){
+button.addEventListener("click",function(event){
+    event.preventDefault();
 
     let num1 = parseInt(document.getElementById("textName").value);
     let num2 = parseInt(document.getElementById("textEmail").value);
@@ -13,13 +14,7 @@ button.addEventListener("click",function(){
     for( let i=num1; i<=num2; i++){
         result+=i;
     }
-    alert(result);
-
+    document.getElementById("textArea").value=result;
 
 })
 
-function rango(num1, num2 ) {
-
-   
-
-  }
