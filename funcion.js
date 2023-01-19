22e60094974d51effbf8f2d55ca2804269340594
Zguedest <zucelys.zge@gmail.com>
2023-01-19 12:02:23 +0100

    sass

--- funcion.js
+++ funcion.js
@@ -2,17 +2,22 @@
 let button= document.getElementById("form_button");
 
 
+
 button.addEventListener("click",function(event){
+
     event.preventDefault();
+    
     let num1 = parseInt(document.getElementById("textName").value);
     let num2 = parseInt(document.getElementById("textEmail").value);
 
     let result=" ";
 
     for( let i=num1; i<=num2; i++){
-        result+=i;
+        result+=i+ " ";
     }
-    document.getElementById("textArea").value=result;
+    //document.getElementById("textArea").value=result;
+
+    document.getElementById("w").innerHTML=result;
 
 })
 
